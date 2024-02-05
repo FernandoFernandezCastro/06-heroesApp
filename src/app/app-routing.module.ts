a8dae10b3e3f18ef3f365fc1e7fab55c714badc6
Fernando Fernandez <flfc161616@gmail.com>
2024-02-05 11:52:51 -0400

    aplicacion final con guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { AuthGuard } from './auth/guards/auth.guards';
+import { PublicGuard } from './auth/guards/public.guards';
 
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import ('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [PublicGuard],
+    canMatch:[PublicGuard]
   },
   {
     path: 'heroes',
--- src/app/auth/guards/public.guards.ts
+++ src/app/auth/guards/public.guards.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+//guard que verifica si el usuario esta autentificado y va a la pagina del login directo entonces se direccione automaticamnete a la lista de heroes
 
-
-//guard que verifica si el usuario esta auntentificado viendo si existe token, si no esta autenticado lo manda a la pantalla de login
 //Debemos hacer notar que este es un servcio que devuelve observables y este Guard se implementa en app-routing-module.ts mediante
 //canMatch o canActivate y si notamos en los procesos de este servicio no hay ningun Subscribe, pero este subscribe lo maneja angular
 //al llamar a este guard en app-routing.module.ts cuando llama a los metodos canActive o canMatch
 //aca solo estamos adicionanod mas tap es decir mas manejos secundarios
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanMatch, CanActivate{
-
+export class PublicGuard implements CanActivate, CanMatch{
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -21,10 +19,11 @@ export class AuthGuard implements CanMatch, CanActivate{
     return this.authService.checkAuthentication()
       .pipe(
         tap(isAuthenticated => {
-          if (!isAuthenticated){
-            this.router.navigate(['./auth/login']);
+          if (isAuthenticated){
+            this.router.navigate(['./']);
           }
-        })
+        }),
+        map( isAuthenticated => !isAuthenticated)
       );
   }
 
@@ -44,5 +43,5 @@ export class AuthGuard implements CanMatch, CanActivate{
   }
 
 
-}
 
+}
